f3ac96594ab3cde4887cf60978f39a69aea137e4
scoin <greg.piccolo@gmail.com>
2014-05-22 14:46:37 -0400

    sql

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -8,26 +8,51 @@
 
 
 # 3. Initial Solution
-
+=begin
 def super_fizzbuzz(array)
 
+array.map{|x| 
+			  add = ''
+			  add = "Fizz" if x % 3 == 0
+			  add += "Buzz" if x % 5 == 0
+			  x = add if add.length > 0 
+			  x
+		 }
+end
+=end
+# 4. Refactored Solution
+def super_fizzbuzz(array)
 
+array.each_with_index{|x, i|
+			 add = ''
+			 add = "Fizz" if x % 3 == 0
+			 add += "Buzz" if x % 5 == 0
+			 array[i] = add if add.length > 0
+}
+array
 end
 
 
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+def random_input_array(base, size)
+  (1..size).map { |i| base**(1+rand(15)) }
+end
 
+p super_fizzbuzz(random_input_array(3,100)) == ["Fizz"]*100
 
+p super_fizzbuzz(random_input_array(5,100)) == ["Buzz"]*100
 
+p super_fizzbuzz(random_input_array(15, 100)) == ["FizzBuzz"]*100
 
+p super_fizzbuzz([1,2,3]) == [1,2,'Fizz']
+
+p super_fizzbuzz([15, 5, 3, 1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1]
 
 
 # 5. Reflection 
+
+#SUPERFIZZBUZZ! Interesting. Regular fizz buzz is easy enough, but the array
+#threw a slight twist. I feel I could make it more elegant, but I did struggle
+#to write it in fewer lines. I tried for a fair amount of time.
+#I've written this program otherwise, and the logic is simple. In this case, it's 
+#structuring the data that is a challenge.
--- week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
@@ -20,23 +20,48 @@ class Rectangle
     (other.width  == self.width && other.height == self.height ) ||
     (other.height == self.width && other.width  == self.height )
   end
+
+  def area
+  	@width * @height
+  end
+
+  def perimeter
+  	(@width * 2) + (@height * 2)
+  end
+
+  def diagonal
+  	Math.sqrt((@width**2).to_f + (height**2).to_f)
+  end
+
+  def square?
+  	@width == @height
+  end
 end
-
-
-
 
 # 4. Refactored Solution
 
 
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+rectangle = Rectangle.new(10, 20)
+square = Rectangle.new(20,20)
 
-
-
-
-
+p rectangle.respond_to?('area') == true
+p rectangle.respond_to?('perimeter') == true
+p rectangle.respond_to?('diagonal') == true
+p rectangle.respond_to?('square?') == true
+p rectangle.area == 200
+p square.area == 400
+p rectangle.perimeter == 60
+p square.perimeter == 80
+p rectangle.diagonal == 22.360679774997898
+p square.diagonal == 28.284271247461902
+p rectangle.square? == false
+p square.square? == true
 
 # 5. Reflection 
+#I am really just picking these at random, but this was kind of a fun one.
+#Good review for syntax, I learned "respond_to?" for the first time. I also
+#learned (not pictured above) that using .class on a method gives you the
+#type it returns - ie. square.area.class returns "Fixnum"
+#Mathematically, I had to look up how to determine diagonal - damn pythagorean theorem!
+
